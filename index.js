0209f7e5b95f9af23f08f198830f5f8d54fe4a3a
Vijay gojiya <61418864+vmgojiya@users.noreply.github.com>
2021-09-09 01:08:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
 
+if(process.env.NODE_ENV == 'production'){
+    app.use(express.static('portfolio'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'portfolio','index.html'))
+    })
+}
+app.listen(port,()=>{
+    console.log("Listning to port "+port);
+})
